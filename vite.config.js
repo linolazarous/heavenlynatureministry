a89bdcc30966744e10f31fe90783ac819f9891f2
linolazarous <linolaku560@gmail.com>
2025-11-10 11:16:37 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Simple configuration - remove all complex options
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    sourcemap: true,
   },
-});
+})
