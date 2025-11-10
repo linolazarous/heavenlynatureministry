37db60535bf88dae28c3a90b86583d5c25223ab8
linolazarous <linolaku560@gmail.com>
2025-11-10 09:33:48 +0200

    Update Donation.jsx

--- src/pages/Donation.jsx
+++ src/pages/Donation.jsx
@@ -23,7 +23,7 @@ import ErrorBoundary from '../components/ErrorBoundary';
 
 // Hooks and Services
 import { useAuth } from '../hooks/useAuth';
-import { donationService } from '../services/donationService;
+import { donationService } from '../services/donationService';
 
 // Constants
 const DEFAULT_TIERS = [
