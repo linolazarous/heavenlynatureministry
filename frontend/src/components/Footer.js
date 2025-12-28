31f56fc21a03d8bb67ba7e61d5229b40ba0cf079
linolazarous <linolaku560@gmail.com>
2025-12-28 08:50:51 +0200

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -38,7 +38,7 @@ const Footer = () => {
               </li>
               <li className="flex items-center gap-2">
                 <Phone className="h-4 w-4" />
-                <span>+1 (555) 123-4567</span>
+                <span>+211 926 006 202</span>
               </li>
               <li className="flex items-center gap-2">
                 <MapPin className="h-4 w-4" />
