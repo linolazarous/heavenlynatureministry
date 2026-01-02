4e719b4106e91465925cd8e7d7831036f73f944f
linolazarous <linolaku560@gmail.com>
2026-01-02 15:42:21 +0200

    Update craco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,6 +1,5 @@
-// craco.config.js
-const path = require("path");
-require("dotenv").config();
+// frontend/craco.config.js
+const path = require('path');
 
 module.exports = {
   webpack: {
@@ -8,40 +7,47 @@ module.exports = {
       '@': path.resolve(__dirname, 'src'),
     },
     configure: (webpackConfig) => {
-      // Remove ESLint plugin from production builds
+      // Remove ESLint plugin for production builds
       if (process.env.NODE_ENV === 'production') {
         webpackConfig.plugins = webpackConfig.plugins.filter(
           (plugin) => plugin.constructor.name !== 'ESLintWebpackPlugin'
         );
       }
       
-      // Add ignored patterns to reduce watched directories
-      webpackConfig.watchOptions = {
-        ...webpackConfig.watchOptions,
-        ignored: [
-          '**/node_modules/**',
-          '**/.git/**',
-          '**/build/**',
-          '**/dist/**',
-          '**/coverage/**',
-          '**/public/**',
-        ],
-      };
+      // Optimize bundle splitting
+      if (process.env.NODE_ENV === 'production') {
+        webpackConfig.optimization = {
+          ...webpackConfig.optimization,
+          splitChunks: {
+            chunks: 'all',
+            cacheGroups: {
+              vendor: {
+                test: /[\\/]node_modules[\\/]/,
+                name: 'vendors',
+                chunks: 'all',
+              },
+            },
+          },
+          runtimeChunk: {
+            name: 'runtime',
+          },
+        };
+      }
       
       return webpackConfig;
     },
   },
   eslint: {
-    enable: process.env.NODE_ENV !== 'production', // Disable ESLint in production
-    mode: 'extends',
-    configure: (eslintConfig) => {
-      eslintConfig.rules = {
-        ...eslintConfig.rules,
-        'no-duplicate-imports': 'off',
-        'no-unused-vars': 'warn',
-        'react/react-in-jsx-scope': 'off',
-      };
-      return eslintConfig;
-    },
+    enable: process.env.NODE_ENV !== 'production',
+  },
+  babel: {
+    presets: [
+      [
+        '@babel/preset-react',
+        {
+          runtime: 'automatic',
+        },
+      ],
+    ],
   },
 };
